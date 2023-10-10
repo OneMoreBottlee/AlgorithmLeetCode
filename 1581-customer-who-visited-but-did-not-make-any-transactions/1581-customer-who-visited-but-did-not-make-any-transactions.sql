71049680571f8a1f297e20eb500d968fdc031e9c
Han ByoungJu <onemorebottlee@gmail.com>
2023-10-10 11:48:09 +0900

    Time: 2073 ms (55.27%), Space: 0B (100.00%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,7 +1,6 @@
 /* Write your PL/SQL query statement below */
 SELECT CUSTOMER_ID, COUNT(*) COUNT_NO_TRANS
 FROM Visits
-WHERE VISIT_ID NOT IN (SELECT DISTINCT v.visit_id
-    FROM Visits V RIGHT JOIN Transactions T
-    ON V.visit_id = T.visit_id)
+WHERE VISIT_ID NOT IN (SELECT DISTINCT visit_id
+    FROM Transactions)
 GROUP BY CUSTOMER_ID
