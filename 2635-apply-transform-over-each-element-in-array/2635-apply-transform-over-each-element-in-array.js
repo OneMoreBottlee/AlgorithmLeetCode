210e4ceef1c90f242db9e60a4062c19f8895b4ae
Han ByoungJu <onemorebottlee@gmail.com>
2023-10-13 11:56:19 +0900

    Time: 50 ms (63.76%), Space: 42.4 MB (6.35%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,5 +4,11 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    return arr.map((el, i) => fn(el, i))
+    const result = []
+    
+    for(let i = 0; i < arr.length; i++){
+        result.push(fn(arr[i], i))
+    }
+    
+    return result
 };
