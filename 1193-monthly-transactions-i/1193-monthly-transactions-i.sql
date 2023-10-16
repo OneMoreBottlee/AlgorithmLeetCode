adbece5dbe950895b1423ddedfae08b8cec069d5
Han ByoungJu <onemorebottlee@gmail.com>
2023-10-16 14:44:03 +0900

    Time: 1012 ms (66.06%), Space: 0B (100.00%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+SELECT TO_CHAR(trans_date, 'YYYY-MM') month, country,
+       COUNT(*) trans_count,
+       SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) approved_count,
+       SUM(amount) trans_total_amount,
+       SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) approved_total_amount
+FROM Transactions
+GROUP BY TO_CHAR(trans_date, 'YYYY-MM'), country
