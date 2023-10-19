8a0e6009298223f8bc82ae2ec54c54b532da7290
Han ByoungJu <onemorebottlee@gmail.com>
2023-10-19 11:10:21 +0900

    Time: 1438 ms (39.42%), Space: 0B (100.00%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,6 @@
+/* Write your PL/SQL query statement below */
+SELECT product_id, year first_year, quantity,price
+FROM Sales
+WHERE (product_id, year) IN (SELECT product_id, MIN(year) first_year
+                             FROM Sales
+                             GROUP BY product_id)
